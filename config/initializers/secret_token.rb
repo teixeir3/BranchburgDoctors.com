f2f3e1d509387fa56b8b6d3cee573bbf2e532ef7
Douglas Teixeira <teixeir3@gmail.com>
2015-03-23 12:21:23 -0400

    Initial commit for BranchburgDoctors.com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BranchburgDoctors::Application.config.secret_key_base = '03597b033c4510276a47b63205d4d518e74a9c86e025d5cc6abb69d0b7ae7f016b37d05fdb358cab7af83f7d7803cfee4dd2d8c8552856754ae46aa42418fb72'
