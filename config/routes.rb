b590373ed874d7c1622806c076714356b53eb8f9
Douglas Teixeira <teixeir3@gmail.com>
2015-04-21 14:08:56 -0400

    Fixes high_voltage pages. Changes styling / formatting. Adds anchor for patient_portal

--- config/routes.rb
+++ config/routes.rb
@@ -15,14 +15,15 @@ BranchburgDoctors::Application.routes.draw do
   resources :downloads, only: [:index]
   resources :observances
   
-  resources :pages
+  # resources :pages
   # get "/pages/*id" => 'pages#resources', as: :page, format: false
   
 
   root to: 'observances#home', id: 'home'
   get 'home', to: redirect('/')
+  
   # root to: "observances#index"
   # root to: "pages#home", as: "home"
-  get '/*id', to:'high_voltage/pages#show'
+  # get '/*id', to:'high_voltage/pages#show'
   # get '/*id', to: 'pages#resources', as: :static, format: false
 end
