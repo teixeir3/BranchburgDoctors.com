bf5280cdc5bc994d5997aa88bab8bdc56a32145d
Douglas Teixeira <teixeir3@gmail.com>
2015-06-01 16:28:11 -0400

    Add new styling to sessions#new.html.erb. Update website layout. Fix error in session_controller#create with routing.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,8 +25,9 @@ class SessionsController < ApplicationController
     if @user
       sign_in(@user)
       flash[:notice] = ["Welcome #{@user.name}"]
-      session[:return_url] = request.referrer unless session[:return_url]
-      redirect_to :back# session[:return_url]
+      # session[:return_url] = request.referrer unless session[:return_url]
+      
+      redirect_to (session[:return_url].nil?) ? :root : session[:return_url]
     else
       flash.now[:errors] = ["Incorrect credentials"]
       render :new
